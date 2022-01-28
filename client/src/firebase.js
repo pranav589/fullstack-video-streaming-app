4f087f9349b0641fe807cd16e18a55507081f28d
pranav589 <64580391+pranav589@users.noreply.github.com>
2022-01-28 19:32:24 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  //firebase config
+  apiKey: "AIzaSyCvdnoda9Ikk-JaupXiFD-6_nmtcoFxqb0",
+  authDomain: "clone-771cc.firebaseapp.com",
+  projectId: "clone-771cc",
+  storageBucket: "clone-771cc.appspot.com",
+  messagingSenderId: "10002172349",
+  appId: "1:10002172349:web:32d54735109fb148a9aad4",
+  measurementId: "G-MEXZL8ZV4N",
 });
 
 const storage = firebase.storage();
